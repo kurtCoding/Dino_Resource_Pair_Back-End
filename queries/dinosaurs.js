a25fc31a3b6bbd2504a72f163ec0751332d0e7ee
franklinTaylor <ftaylor07@icloud.com>
2024-07-22 19:53:51 -0400

    added route and query for single dino

--- queries/dinosaurs.js
+++ queries/dinosaurs.js
@@ -9,4 +9,21 @@ const getAllDinos = async () => {
     }
 };
 
-module.exports = { getAllDinos };
+//get dino by name
+const getDino = async (name) => {
+    const query = 'SELECT * FROM dinosaurs WHERE name = $1';
+  
+    try {
+        const dino = await db.oneOrNone(query, [name]);
+        return dino;
+    } catch (err) {
+        console.error('Cant Find Dino try Again', err);
+        return null;
+    }
+};
+
+module.exports = {
+    getAllDinos,
+    getDino
+
+}
